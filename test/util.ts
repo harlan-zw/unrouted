6a26f9d14857f09a38a906e730f166bcc2d24635
Harlan Wilton <harlan@harlanzw.com>
2022-01-29 13:26:33 +1100

    chore(mono): lint

--- test/util.ts
+++ test/util.ts
@@ -1,7 +1,7 @@
 import testKit from '@unrouted/test-kit'
-import type { RequestPathSchema } from '../api-routes'
 import createApi from './fixtures/api/myApi'
 import * as assertions from './assertions'
+import type { RequestPathSchema } from './fixtures/api/__routes__/api-routes'
 
 export async function bootstrap(app: any) {
   const api = await createApi({ debug: true })
